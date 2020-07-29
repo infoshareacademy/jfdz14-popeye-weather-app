2ff2ed702c4cd633f6fde5e6a360b3071e272fae
Agnieszka Lewicka <aelewicka@gmail.com>
2020-07-29 20:04:12 +0200

    edited result item for stacja view

--- src/ComponentsDashboard/SearchResulItem.jsx
+++ src/ComponentsDashboard/SearchResulItem.jsx
@@ -1,20 +1,44 @@
 import React from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 import { AppContent } from "./AppContent";
+import Table from 'react-bootstrap/Table';
+import { CodeSharp } from "@material-ui/icons";
 
 
 
 class SearchResultItem extends React.Component {
 
-    state= {
-        stacja: null
+    state = {
+        stacja: []
     }
 
+
+    componentDidMount() {
+        fetch("https://danepubliczne.imgw.pl/api/data/synop")
+            .then((response) => response.json())
+            .then((stacja) => 
+                this.setState({
+                  stacja
+                })
+            );
+            // console.log(this.state.stacja)
+    };
+
+
     render() {
-        return <AppContent>
-            <div><h2>kot</h2></div>
-        </AppContent>
+        return (
+            <AppContent>
+                <Table striped bordered hover>
+                    <div><h2>kot</h2></div>
+                    <div></div>
+                    {/* {this.state.stacja.find(stacja =>
+                        stacja.id_stacji === this.props.match.params.id)} */}
+                </Table>
+
+            </AppContent>)
+        // console.log(this.state.stacja.find(stacja => stacja.id_stacji === this.props.match.params.id))
     }
+
 }
 
 export default SearchResultItem;
