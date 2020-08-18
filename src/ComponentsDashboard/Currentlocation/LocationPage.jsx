2e5913fe24f43a6f0e57c34e0021d323f0a1193f
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2020-08-18 12:45:21 +0200

    working currentLocation weather

--- src/ComponentsDashboard/Currentlocation/LocationPage.jsx
+++ src/ComponentsDashboard/Currentlocation/LocationPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AppContent } from '../Dashboards/AppContent';
 import style from './currentLocation.module.css';
-// import SearchResultItem from '../SearchPages/SearchResulItem';
+import SearchResultItem from '../SearchPages/SearchResulItem';
 import { getWeatherForLocation } from '../../datasources/weatherForLocation';
 
 // export function LocationPage() {
@@ -14,6 +14,11 @@ class LocationPage extends React.Component {
   state = {
     long: null,
     lat: null,
+    temperature: null,
+    pressure: null,
+    windSpeed: null,
+    humidity: null,
+    precipitation: null,
   };
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(position => {
@@ -22,22 +27,35 @@ class LocationPage extends React.Component {
           long: position.coords.longitude,
           lat: position.coords.latitude,
         },
-        getWeatherForLocation(this.state.long, this.stare.lat),
+        () =>
+          getWeatherForLocation(this.state.long, this.state.lat).then(data =>
+            // data => console.log(data.precipitation),
+            this.setState({
+              temperature: `${(data.temperature - 273.15).toFixed(0)} ℃`,
+              pressure: `${(data.pressure / 100).toFixed(0)} hPa`,
+              humidity: `${data.humidity.toFixed(1)} %`,
+              windSpeed: `${data.windSpeed.toFixed(2)} m/s`,
+              precipitation: `${data.precipitation} mm`,
+            }),
+          ),
       );
     });
   }
-
   render() {
     return (
       <AppContent>
         <div>
           <h4>
-            {this.state.long ? (
+            {/* {this.state.long}, {this.state.lat},  */}
+            {this.state.temperature}, {this.state.humidity}, {this.state.windSpeed},
+            {this.state.precipitation}, {this.state.pressure}
+            {/* <SearchResultItem long={this.state.long} lat={this.state.lat} /> */}
+            {/* {this.state.long ? (
               // `${this.state.long} , ${this.state.lat}`
               <SearchResultItem long={this.state.long} lat={this.state.lat} />
-            ) : (
-              <h2>You did not allow to see your position</h2>
-            )}
+            ) : ( */}
+            {/* <h2>You did not allow to see your position</h2> */}
+            {/* )} */}
           </h4>
         </div>
       </AppContent>
