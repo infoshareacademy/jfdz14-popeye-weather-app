c5863cbfac24fa17830f3face81edd64969d1c52
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2020-08-18 11:03:53 +0200

    added geolocation in currentLocation Page

--- src/ComponentsDashboard/Currentlocation/LocationPage.jsx
+++ src/ComponentsDashboard/Currentlocation/LocationPage.jsx
@@ -1,8 +1,48 @@
 import React from 'react';
 import { AppContent } from '../Dashboards/AppContent';
 import style from './currentLocation.module.css';
-export function LocationPage() {
-  return <AppContent>
-    <h3>Current Location</h3>
-  </AppContent>;
+// import SearchResultItem from '../SearchPages/SearchResulItem';
+import { getWeatherForLocation } from '../../datasources/weatherForLocation';
+
+// export function LocationPage() {
+//   return <AppContent>
+//     <h3>Current Location</h3>
+//   </AppContent>;
+// }
+
+class LocationPage extends React.Component {
+  state = {
+    long: null,
+    lat: null,
+  };
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(position => {
+      this.setState(
+        {
+          long: position.coords.longitude,
+          lat: position.coords.latitude,
+        },
+        getWeatherForLocation(this.state.long, this.stare.lat),
+      );
+    });
+  }
+
+  render() {
+    return (
+      <AppContent>
+        <div>
+          <h4>
+            {this.state.long ? (
+              // `${this.state.long} , ${this.state.lat}`
+              <SearchResultItem long={this.state.long} lat={this.state.lat} />
+            ) : (
+              <h2>You did not allow to see your position</h2>
+            )}
+          </h4>
+        </div>
+      </AppContent>
+    );
+  }
 }
+
+export default LocationPage;
