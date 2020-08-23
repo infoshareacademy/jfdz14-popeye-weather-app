c0ff3b6e157536211b3f868fcc5fa1ac2557ae76
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2020-08-23 19:06:58 +0200

    promise all with fetch

--- src/datasources/weatherForLocation.js
+++ src/datasources/weatherForLocation.js
@@ -1,30 +1,42 @@
-export function getWeatherForLocation(long, lat) {
-  return fetch('https://api.windy.com/api/point-forecast/v2', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      lat: lat,
-      lon: long,
-      model: 'gfs',
-      parameters: ['rh', 'pressure', 'temp', 'windGust', 'ptype'],
-      levels: ['surface'],
-      key: 'm7zU1cmERbDVXbKfHCM3BW3z56DIoah0',
-    }),
-  })
+export const getWeatherForLocation = (lon, lat) => {
+  return fetch(
+    'https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly&appid=61a970cfc56f94d7136cb0eec9205048',
+  )
     .then(r => r.json())
     .then(data => {
-      return {
-        humidity: data['rh-surface'][0],
-        temperature: data['temp-surface'][0],
-        pressure: data['pressure-surface'][0],
-        windSpeed: data['gust-surface'][0],
-        precipitation: data['ptype-surface'][0],
-      };
+      console.log(data);
+
+      return data;
     });
-}
+};
+
+// export function getWeatherForLocation(long, lat) {
+//   return fetch('https://api.windy.com/api/point-forecast/v2', {
+//     method: 'POST',
+//     headers: {
+//       Accept: 'application/json',
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({
+//       lat: lat,
+//       lon: long,
+//       model: 'gfs',
+//       parameters: ['rh', 'pressure', 'temp', 'windGust', 'ptype'],
+//       levels: ['surface'],
+//       key: 'm7zU1cmERbDVXbKfHCM3BW3z56DIoah0',
+//     }),
+//   })
+//     .then(r => r.json())
+//     .then(data => {
+//       return {
+//         humidity: data['rh-surface'][0],
+//         temperature: data['temp-surface'][0],
+//         pressure: data['pressure-surface'][0],
+//         windSpeed: data['gust-surface'][0],
+//         precipitation: data['ptype-surface'][0],
+//       };
+//     });
+// }
 
 // const getWeatherData = (long, lat) => {
 //   fetch('https://api.windy.com/api/point-forecast/v2', {
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './fonts/Baloo2-Regular.ttf';
 import firebase from 'firebase';
+import { cities } from './datasources/cities';
 
 // const firebaseConfig = {
 //   apiKey: "AIzaSyBIe1o8mvKeSjXHT0PSwko0Y27o7flM3Bg",
@@ -16,6 +17,30 @@ import firebase from 'firebase';
 //   appId: "1:824948431301:web:8fa0e89a8bb5a1c03793ac",
 //   measurementId: "G-CHWD30XWMZ"
 // };
+
+cities.map(city => {
+  return fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${city.lat}&lon=${city.long}&exclude=hourly,minutely&appid=61a970cfc56f94d7136cb0eec9205048`,
+  )
+    .then(r => r.json())
+    .then(data => {
+      console.log(data);
+      return data;
+    });
+});
+
+const allDataFromFetch = Promise.all([cities]).then();
+
+export const getWeatherForLocation = (lon, lat) => {
+  return fetch(
+    'https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly&appid=61a970cfc56f94d7136cb0eec9205048',
+  )
+    .then(r => r.json())
+    .then(data => {
+      console.log(data);
+      return data;
+    });
+};
 
 export const DATABASE_URL = 'https://popyeweather-352f0.firebaseio.com';
 
