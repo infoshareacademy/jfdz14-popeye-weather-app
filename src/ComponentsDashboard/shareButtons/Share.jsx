11c9f02018740da6bc5876f3dc4a924eb159bba5
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2020-09-01 21:27:25 +0200

    share buttons styled

--- src/ComponentsDashboard/shareButtons/Share.jsx
+++ src/ComponentsDashboard/shareButtons/Share.jsx
@@ -6,22 +6,34 @@ import {
   FacebookShareCount,
   EmailShareButton,
   EmailIcon,
+  TwitterShareButton,
+  TwitterIcon,
 } from 'react-share';
 
 const ShareButtons = ({ isHovered }) => (
   <div
-    className={`${style.shareButtonsContainer}, ${isHovered && style.shareButtonsContainerHovered}`}
-    // isHovered={this.handleMouseHover}
-    // onMouseLeave={this.handleMouseHover}
+    className={`${style.shareButtonsContainer} ${isHovered && style.shareButtonsContainerHovered}`}
   >
     <FacebookShareButton
       url="http://app.popeyeweather.jfdz14.is-academy.pl/"
-      quote={'Hey! Check out the best weather app in the World ! '}
+      quote={'Hey! Checkout the best weather app in the World ! '}
       hashtag="#PopeyeWeatherApp"
     >
       <FacebookIcon logoFillColor="transparent" size={32} round={true} />
+      <FacebookShareCount url={'http://app.popeyeweather.jfdz14.is-academy.pl/'} />
     </FacebookShareButton>
-    <EmailShareButton>
+    <TwitterShareButton
+      url="http://app.popeyeweather.jfdz14.is-academy.pl/"
+      title="Hey! Checkout the best weather app in the World !"
+      hashtags={['#PopeyeWeatherApp', '#weather', '#sunny', '#forecast']}
+      // via="http://app.popeyeweather.jfdz14.is-academy.pl/"
+    >
+      <TwitterIcon size={32} round={true} />
+    </TwitterShareButton>
+    <EmailShareButton
+      subject="Checkout the World's best weather App - Popeye Weather"
+      body="In Popeye Weather application you will find the most up-to-date weather details. Try it at: http://app.popeyeweather.jfdz14.is-academy.pl/"
+    >
       <EmailIcon size={32} round={true} />
     </EmailShareButton>
   </div>
