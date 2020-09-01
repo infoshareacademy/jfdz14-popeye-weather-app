d6b61269083f08066bbd5ce7049eb7dcf7819b02
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2020-09-01 21:38:35 +0200

    share buttons done

--- src/ComponentsDashboard/shareButtons/Share.jsx
+++ src/ComponentsDashboard/shareButtons/Share.jsx
@@ -3,33 +3,42 @@ import style from './share.module.css';
 import {
   FacebookIcon,
   FacebookShareButton,
-  FacebookShareCount,
   EmailShareButton,
   EmailIcon,
   TwitterShareButton,
   TwitterIcon,
+  LinkedinShareButton,
+  LinkedinIcon,
 } from 'react-share';
+
+const pageUrl = 'http://app.popeyeweather.jfdz14.is-academy.pl/';
 
 const ShareButtons = ({ isHovered }) => (
   <div
     className={`${style.shareButtonsContainer} ${isHovered && style.shareButtonsContainerHovered}`}
   >
     <FacebookShareButton
-      url="http://app.popeyeweather.jfdz14.is-academy.pl/"
+      url={pageUrl}
       quote={'Hey! Checkout the best weather app in the World ! '}
       hashtag="#PopeyeWeatherApp"
     >
       <FacebookIcon logoFillColor="transparent" size={32} round={true} />
-      <FacebookShareCount url={'http://app.popeyeweather.jfdz14.is-academy.pl/'} />
     </FacebookShareButton>
     <TwitterShareButton
-      url="http://app.popeyeweather.jfdz14.is-academy.pl/"
+      url={pageUrl}
       title="Hey! Checkout the best weather app in the World !"
       hashtags={['#PopeyeWeatherApp', '#weather', '#sunny', '#forecast']}
       // via="http://app.popeyeweather.jfdz14.is-academy.pl/"
     >
       <TwitterIcon size={32} round={true} />
     </TwitterShareButton>
+    <LinkedinShareButton
+      url={pageUrl}
+      title="Hey! Checkout the best weather app in the World !"
+      summary="In Popeye Weather application you will find the most up-to-date weather details."
+    >
+      <LinkedinIcon size={32} round={true} />
+    </LinkedinShareButton>
     <EmailShareButton
       subject="Checkout the World's best weather App - Popeye Weather"
       body="In Popeye Weather application you will find the most up-to-date weather details. Try it at: http://app.popeyeweather.jfdz14.is-academy.pl/"
