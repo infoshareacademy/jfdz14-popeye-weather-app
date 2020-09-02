8399a4a530c943f7985e22fcd0363da9d1e8866a
Agnieszka Lewicka <aelewicka@gmail.com>
2020-09-02 23:10:28 +0200

    SignInPage fixed redirection

--- src/ComponentsDashboard/LoginPages/SignInPage.jsx
+++ src/ComponentsDashboard/LoginPages/SignInPage.jsx
@@ -10,6 +10,11 @@ import BackButton from './BackButton';
 
 import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
+
+import { Redirect, Link } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import PageWrapper from "./PageWrapper";
 import firebase from "firebase";
 
 // const SignInPage = () => {
@@ -45,50 +50,102 @@ class SignInPage extends React.Component {
   handleOnSubmit = (event) => {
     event.preventDefault();
 
-    firebase.auth()
-      .signInWithEmailAndPassword(this.state.email, this.state.password)
-      .then((userData) => {
-        this.setState({
-          redirect: true
+    if (this.props.isSignUp) {
+      firebase.auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(() => {
+          this.setState({
+            redirect: true
+          })
         })
-      })
-      .catch((error) => {
-        alert(error.message);
-      })
+        .catch((error) => {
+          alert(error.message);
+        })
+    } else {
+      firebase.auth()
+        .signInWithEmailAndPassword(this.state.email, this.state.password)
+        .then((userData) => {
+          // console.log(userData)
+          this.setState({
+            redirect: true
+          })
+        })
+        .catch((error) => {
+          alert(error.message);
+        })
+    }
   }
 
-  
+
   render() {
     if (this.state.redirect) {
-      return <Redirect to='/' />
+      return <Redirect to='/home' />
     }
+
+    // return (
+    //   <div className={style.loginPage} onSubmit={this.handleOnSubmit}>
+    //     <PopeyeImg />
+    //     <NameOfApp paragraph={'Sign in to use application'} />
+    //     <Input
+    //       type={'text'}
+    //       placeholder={'Enter your email address'}
+    //       onChange={this.handleOnChange}
+    //       value={this.state.email}
+    //     />
+    //     <Divider />
+    //     <Input
+    //       type={'password'}
+    //       placeholder={'Enter your password'}
+    //       onChange={this.handleOnChange}
+    //       value={this.state.password}
+    //     />
+    //     <LoginButton
+    //       toLogIn text={'Sign In'}
+    //       urlName={'home'}
+    //     />
+    //     <BackButton />
+    //     <LoginPageFooter />
+    //   </div>
+    // );
+
+
 
     return (
       <div className={style.loginPage}>
         <PopeyeImg />
-        <NameOfApp paragraph={'Sign in to use application'} />
-        <Input
-          type={'text'}
-          placeholder={'Enter your email address'}
-          onChange={this.handleOnChange}
-          value={this.state.email}
-        />
-        <Divider />
-        <Input
-          type={'password'}
-          placeholder={'Enter your password'}
-          onChange={this.handleOnChange}
-          value={this.state.password}
-        />
-        <LoginButton
-          toLogIn text={'Sign In'}
-          urlName={'home'}
-          onSubmit={this.handleOnSubmit}
-        />
-        <BackButton />
-        <LoginPageFooter />
+        <PageWrapper title={this.props.isSignUp ? 'Register to use application' : 'Sign in to use application'}>
+          <Form className='m-4 text-left' onSubmit={this.handleOnSubmit}>
+            <Form.Group controlId="formGroupEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter your email address"
+                name='email'
+                value={this.state.email}
+                onChange={this.handleOnChange} />
+            </Form.Group>
+            <Form.Group controlId="formGroupPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter your password"
+                name='password'
+                value={this.state.password}
+                onChange={this.handleOnChange} />
+            </Form.Group>
+            <Button variant='primary' type='submit'>
+              {this.props.isSignUp ? 'Register to use application' : 'Sign In'}
+            </Button>
+            <BackButton />
+            {
+              this.props.isSignUp
+                ? <Link to='/sign-in'><Button variant='link' type='submit' className='ml-3'>Already have an account? Sign In</Button></Link>
+                : <Link to='/sign-up'><Button variant='link' type='submit' className='ml-3'>Don't have an account? Sign Up</Button></Link>
+            }
+          </Form>
+        </PageWrapper>
       </div>
-    );
+    )
 
 
   }
