c29d6f7de5b25c88294626869833be7a044fb46e
Agnieszka Lewicka <aelewicka@gmail.com>
2020-09-02 22:23:16 +0200

    SignInPage updated with firebase

--- src/ComponentsDashboard/LoginPages/Input.jsx
+++ src/ComponentsDashboard/LoginPages/Input.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import style from './LoginPage.module.css';
 
-export const Input = ({ type, placeholder }) => {
-  return <input className={style.input} type={type} placeholder={placeholder}></input>;
+export const Input = ({ type, placeholder, onChange, value }) => {
+  return <input className={style.input} type={type} placeholder={placeholder} onChnage={onChange} value={value}></input>;
 };
--- src/ComponentsDashboard/LoginPages/SignInPage.jsx
+++ src/ComponentsDashboard/LoginPages/SignInPage.jsx
@@ -8,19 +8,90 @@ import { NameOfApp } from '../Dashboards/NameOfApp';
 import { Divider } from '@material-ui/core';
 import BackButton from './BackButton';
 
-const SignInPage = () => {
-  return (
-    <div className={style.loginPage}>
-      <PopeyeImg />
-      <NameOfApp paragraph={'Sign in to use application'} />
-      <Input type={'text'} placeholder={'Enter your email address'} />
-      <Divider />
-      <Input type={'password'} placeholder={'Enter your password'} />
-      <LoginButton toLogIn text={'Sign In'} urlName={'home'} />
-      <BackButton />
-      <LoginPageFooter />
-    </div>
-  );
-};
+import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import firebase from "firebase";
+
+// const SignInPage = () => {
+//   return (
+//     <div className={style.loginPage}>
+//       <PopeyeImg />
+//       <NameOfApp paragraph={'Sign in to use application'} />
+//       <Input type={'text'} placeholder={'Enter your email address'} />
+//       <Divider />
+//       <Input type={'password'} placeholder={'Enter your password'} />
+//       <LoginButton toLogIn text={'Sign In'} urlName={'home'} />
+//       <BackButton />
+//       <LoginPageFooter />
+//     </div>
+//   );
+// };
+
+
+class SignInPage extends React.Component {
+  state = {
+    email: "",
+    password: "",
+    redirect: false,
+  }
+
+  handleOnChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+
+    firebase.auth()
+      .signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then((userData) => {
+        this.setState({
+          redirect: true
+        })
+      })
+      .catch((error) => {
+        alert(error.message);
+      })
+  }
+
+  
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to='/' />
+    }
+
+    return (
+      <div className={style.loginPage}>
+        <PopeyeImg />
+        <NameOfApp paragraph={'Sign in to use application'} />
+        <Input
+          type={'text'}
+          placeholder={'Enter your email address'}
+          onChange={this.handleOnChange}
+          value={this.state.email}
+        />
+        <Divider />
+        <Input
+          type={'password'}
+          placeholder={'Enter your password'}
+          onChange={this.handleOnChange}
+          value={this.state.password}
+        />
+        <LoginButton
+          toLogIn text={'Sign In'}
+          urlName={'home'}
+          onSubmit={this.handleOnSubmit}
+        />
+        <BackButton />
+        <LoginPageFooter />
+      </div>
+    );
+
+
+  }
+}
 
 export default SignInPage;
