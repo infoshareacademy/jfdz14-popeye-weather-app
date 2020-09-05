998bc79a9196c673226a17a5435b8d553e0491b7
Kasia <kdmitrus@wp.pl>
2020-09-05 16:17:29 +0200

    poprawki miast

--- src/datasources/cities.js
+++ src/datasources/cities.js
@@ -48,13 +48,13 @@ export const cities = [
     image: '',
   },
   //Litwa
-  { name: 'Kłajpeda', country: 'Lithuania', long: 20.5972608, lat: 55.7040636, image: '' },
+  { name: 'Klaipėda', country: 'Lithuania', long: 20.5972608, lat: 55.7040636, image: '' },
   //Łotwa
-  { name: 'Liepaja', country: 'Latvia', long: 20.7582523, lat: 56.5366303, image: '' },
+  { name: 'Liepāja', country: 'Latvia', long: 20.7582523, lat: 56.5366303, image: '' },
   { name: 'Ventspils', country: 'Latvia', long: 21.3144645, lat: 57.4090969, image: '' },
-  { name: 'Riga', country: 'Latvia', long: 23.5685424, lat: 56.9702211, image: '' },
+  { name: 'Rīga', country: 'Latvia', long: 23.5685424, lat: 56.9702211, image: '' },
   //Estonia
-  { name: 'Parnu', country: 'Estonia', long: 24.3877609, lat: 58.3789255, image: '' },
+  { name: 'Pärnu', country: 'Estonia', long: 24.3877609, lat: 58.3789255, image: '' },
   { name: 'Tallinn', country: 'Estonia', long: 24.1776447, lat: 59.470493, image: '' },
   { name: 'Saaremaa', country: 'Estonia', long: 20.3327323, lat: 58.2535706, image: '' },
   //Finland
@@ -63,7 +63,7 @@ export const cities = [
   { name: 'Oulu', country: 'Finland', long: 23.1984144, lat: 65.1875808, image: '' },
   //sweden
   { name: 'Stockholm', country: 'Sweden', long: 17.4214544, lat: 59.3249386, image: '' },
-  { name: 'Malmo', country: 'Sweden', long: 12.7357374, lat: 55.5698489, image: '' },
+  { name: 'Malmö', country: 'Sweden', long: 12.7357374, lat: 55.5698489, image: '' },
   { name: 'Kalmar', country: 'Sweden', long: 16.047557, lat: 56.6871289, image: '' },
   { name: 'Gotland', country: 'Sweden', long: 14.1541633, lat: 57.5715131, image: '' },
   { name: 'Öland', country: 'Sweden', long: 14.3270924, lat: 57.959666, image: '' },
